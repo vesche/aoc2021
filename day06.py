5b97488eee2fa0d5bdad3355b60413e4d2042c73
vesche <vesche@github.com>
2021-12-06 00:01:21 -0600

    py day 6 edit

--- day06.py
+++ day06.py
@@ -4,9 +4,8 @@ with open('inputs/day06.txt') as f:
 lf = [data.count(n) for n in range(9)]
 
 for step in range(256):
-    x = lf[0]
-    lf[7] += x
-    lf = lf[1:] + [x]
+    lf[7] += lf[0]
+    lf = lf[1:] + [lf[0]]
     if step+1 == 80:
         print('part 1:', sum(lf))
 
